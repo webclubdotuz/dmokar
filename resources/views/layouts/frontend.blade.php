7bdd210dc65b5d7bb0297fec52d7b07f7d76f0f8
Mambetali Nokisbaev <webclubuz@gmail.com>
2024-10-22 16:30:36 +0500

    123

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <title>DMO Karakalpakstan</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Free HTML Templates" name="keywords">
-    <meta content="Free HTML Templates" name="description">
+    <meta content="DMO Karakalpakstan" name="keywords">
+    <meta content="DMO Karakalpakstan" name="description">
 
 
     <!-- Favicon -->
